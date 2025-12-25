79f10a8dd3c5cdf8ee29885472f9c5314424fd35
Sahil Shahaji Kadam <kadamsahil2511@gmail.com>
2025-12-25 12:16:45 +0530

    depoyment try 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
+import webpack from 'next/dist/compiled/webpack/webpack-lib.js';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve = config.resolve || {}
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
@@ -8,8 +10,26 @@ const nextConfig = {
       '@react-native-async-storage/async-storage': false,
       'pino-pretty': false,
     }
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      }
+    }
+
+    // Ignore the porto module's problematic chain imports
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^porto$/,
+      })
+    )
+
     return config
   },
 };
 
 export default nextConfig;
+
